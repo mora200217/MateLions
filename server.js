34a35fce048de8d05a78aa0f515e8d768f671601
Andrés Morales <mora200217@hotmail.com>
2018-05-13 21:39:38 -0500

    Dependencies installed. Example server

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 // Created by Andres Morales
 // Main Server App Control
 
+var express = require('express');
+var app = express();
 
-function initServer(){
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
-}
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
